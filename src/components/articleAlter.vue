0d681617caaa4644099deb5434ce20798b8f3cb6
gansiqiu <1223720954@qq.com>
2018-09-20 10:10:18 +0800

    后台静态界面

---
+++ src/components/articleAlter.vue
@@ -0,0 +1,56 @@
+<template lang="html">
+  	<div class="articleAlter">
+		<Breadcrumb>
+  			<BreadcrumbItem to="/articleManage">
+  				<Icon type="ios-build" size="24"/>好文管理
+  			</BreadcrumbItem>
+			<BreadcrumbItem>
+  				<Icon type="md-add" size="24"/>新建好文
+  			</BreadcrumbItem>
+  		</Breadcrumb><br />
+		<Form :model="formItem" :label-width="150">
+		   	<FormItem label="好文文章名:">
+			   	<Input v-model="formItem.articleName" placeholder="请输入文章名" clearable></Input>
+		   	</FormItem>
+			<FormItem label="文章类型:">
+	            <Select v-model="formItem.articleType" placeholder="选择类型">
+	                <Option value="1">New York</Option>
+	                <Option value="2">London</Option>
+	                <Option value="3">Sydney</Option>
+	            </Select>
+	        </FormItem>
+			<FormItem>
+				<Button type="primary" long @click="submitClick">提交</Button>
+			</FormItem>
+		</Form>
+  	</div>
+</template>
+
+<script>
+export default {
+	name:"articleAlter",
+	data(){
+		return{
+			formItem:{
+				articleName:"",
+				articleType:"1"
+			}
+		}
+	},
+	methods:{
+		submitClick(){
+			console.log("submit");
+		}
+	}
+}
+</script>
+
+<style lang="css">
+.articleAlter{
+	padding: 20px;
+}
+form{
+	width: 70%;
+	margin: 0 auto;
+}
+</style>
---
+++ src/components/videoType.vue
@@ -0,0 +1,74 @@
+<template lang="html">
+	<div class="videoType">
+		<Breadcrumb>
+	        <BreadcrumbItem>
+	            <Icon type="ios-build" size="24"/>视频类别管理
+	        </BreadcrumbItem>
+	    </Breadcrumb><br />
+		<Button icon="md-add" type="primary" @click="newType">新建</Button><br /><br />
+		<Table :columns="columns" :data="dataList"></Table><br />
+		<Page :total="total" show-total/>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"videoType",
+    data () {
+        return {
+			total:100,
+			columns:[
+				{
+                    title: 'Name',
+                    key: 'name'
+                },
+                {
+                    title: 'Age',
+                    key: 'age'
+                },
+                {
+                    title: 'Address',
+                    key: 'address'
+                }
+			],
+			dataList: [
+                {
+                    name: 'John Brown',
+                    age: 18,
+                    address: 'New York No. 1 Lake Park',
+                    date: '2016-10-03'
+                },
+                {
+                    name: 'Jim Green',
+                    age: 24,
+                    address: 'London No. 1 Lake Park',
+                    date: '2016-10-01'
+                },
+                {
+                    name: 'Joe Black',
+                    age: 30,
+                    address: 'Sydney No. 1 Lake Park',
+                    date: '2016-10-02'
+                },
+                {
+                    name: 'Jon Snow',
+                    age: 26,
+                    address: 'Ottawa No. 2 Lake Park',
+                    date: '2016-10-04'
+                }
+            ]
+        }
+    },
+    methods: {
+		newType(){
+			this.$router.push({name:"videoTypeAlter"});
+		}
+    }
+}
+</script>
+
+<style lang="css">
+.videoType{
+	padding: 20px;
+}
+</style>
